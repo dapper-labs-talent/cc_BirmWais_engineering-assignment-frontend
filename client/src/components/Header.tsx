c71aec9bb9dd4bd4cd9ff67c162c7b450639b3e6
Birm Wais <birmwais@Birms-MacBook-Pro.local>
2022-06-22 22:00:39 -0600

    added header jsx

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -6,17 +6,35 @@ interface Props {
 
 const Header: React.FC<Props> = (): JSX.Element => {
 
+	const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+		console.log("selected option:", e.target.value)
+	}
+
 	return (
 		<header>
 			<h1>Upcoming Drops</h1>
 			<div className="sort">
 				<label htmlFor="sort-by">Sort By</label>
-				<select name="sort-by" id="sort-by">
-					
+				<select name="sort-by" id="sort-by" onChange={handleSelect}>
+					{sortOptions.map(({value, label}) => {
+						return <option value={value} key={label}>{label}</option>;
+					})}
 				</select>
 			</div>
 		</header>
 	);
 }
+
+const sortOptions = [
+	{
+		label: "Newest First",
+		value: "newest",
+	},
+	{
+		label: "Oldest First",
+		value: "oldest",
+	}
+];
+
 
 export default Header;
