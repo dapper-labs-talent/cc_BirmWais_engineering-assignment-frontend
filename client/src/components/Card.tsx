02c21f392a3c232558edb7976890611504a71a83
Birm Wais <birmwais@Birms-MacBook-Pro.local>
2022-06-26 17:26:52 -0600

    fix position of image

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -12,23 +12,28 @@ const Card: React.FC<Props> = (props): JSX.Element => {
 	return (
 		<div className="card">
 
-			<div className="card-shadow">
+			<div className="image-container">
+
 				<img src={image} alt={`${name} photo`} />
-				<div className="card-header">
-					<div className="drop-date">
-						<p className="date">{dropDate}</p>
-						<p className="description">Drop Date</p>
+
+				<div className="card-shadow">
+					<div className="card-header">
+						<div className="drop-date">
+							<p className="date">{dropDate}</p>
+							<p className="description">Drop Date</p>
+						</div>
+						<span className="icon">{icon}</span>
 					</div>
-					<span className="icon">{icon}</span>
+
+					<div className="card-body">
+						<h4>{name}</h4>
+						<div className="link">
+							<span>;& </span>
+							<a href={website}>{website}</a>
+						</div>
+					</div>
 				</div>
 
-				<div className="card-body">
-					<h4>{name}</h4>
-					<div className="link">
-						<span>;& </span>
-						<a href={website}>{website}</a>
-					</div>
-				</div>
 			</div>
 
 			<div className="card-athletes-footer">
